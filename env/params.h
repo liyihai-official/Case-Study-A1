44571209f918bcb78d1713c2005d2fc9aa8f8880
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-19 13:41:29 +0800

    first.commit

---
+++ env/params.h
@@ -0,0 +1,59 @@
+///
+/// @file params.h
+/// @brief This file contains key parameters for the project.
+///         Vector size is N x 1
+///         Matrix size is M x N
+///         Datatype of elements is float64 aka double
+/// @author LI YIHAI
+/// @version 0.1
+/// @date 2026-01-19 
+///
+#ifndef PARAMS_H_LI
+#define PARAMS_H_LI
+
+/// includes 
+#include <cstdint>  // C Standard Integer types
+#include <string>   // 'std::string' type
+#include <iostream> // Necessary IO streams
+
+///
+/// V: Vector size
+/// N: Matrix Col size
+/// M: Matrix Row size
+/// 
+#undef V
+#undef N
+#undef M
+
+#define V 100   // Vector size
+#define N 1000  // Matrix Col size
+#define M 10000 // Matrix Row 
+
+
+///
+/// Datatype of elements
+///
+namespace tQR_project
+{
+  // Type Aliases
+  using Byte  = uint8_t;
+  using Word  = uint16_t;
+  using Dword = uint32_t;
+  using Qword = uint64_t;
+
+  // Types
+  using Int = int;
+  using UnsignedInt = unsigned int;
+  using Float       = float;
+  using Double      = double;
+  using Char        = char;
+  using Bool        = bool;
+  using String      = std::string;
+
+  // Define types
+  typedef Dword   size_type;
+  typedef Double  elem_type;
+} // end of namespace tQR_project
+
+
+#endif // end of define PARAMS_H_LI
---
+++ lib/datahandler.h
@@ -0,0 +1,16 @@
+#if !defined(DATAHANDLER_H)
+
+#define DATAHANDLER_H
+#include <vector>
+#include "../env/params.h"
+
+class DataHandler {
+public:
+
+
+private:
+
+};
+
+
+#endif // DATAHANDLER_H
