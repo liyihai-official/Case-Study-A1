4a8ec5f456cdaf6bb5e44ed696eabf10350a7015
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-19 23:34:48 +0800

    add.main

---
+++ main.cc
@@ -0,0 +1,15 @@
+/// 
+/// 
+/// @file main.cc
+/// @brief Main program for Tall Skinny QR Factorization, 
+///         for solving Ax=b linear equations.
+///
+/// @author LI YIHAI
+/// @version 0.1
+/// @date 2026-01-19 
+///
+
+int main ()
+{
+  return 0;
+}
