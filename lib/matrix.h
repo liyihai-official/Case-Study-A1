858543d5b39bb689f52043fb59428b8b9416d8a0
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-20 21:55:34 +0800

    DEBUG.mode

--- lib/matrix.h
+++ lib/matrix.h
@@ -15,20 +15,38 @@
 #include "../env/params.h"
 #include "datahandler.h"
 
-
+///
 /// @brief Matrix class
 ///         Matrix class constructed from DataHandler object
 ///         Data stored in a 1D vector internally
 ///         Provides necessary Matrix operations (to be implemented)
-///
+/// @todo Implement more member functions as needed 
 class Matrix {
 public:
-  Matrix() { std::cout << "Matrix default constructor called." << std::endl; }; // default constructor
-  Matrix(const Matrix&);            // copy constructor
-  Matrix& operator=(const Matrix&); // copy assignment
+  
+  Matrix() // default constructor
+  { 
+    #ifdef DEBUG_LOG
+    std::cout 
+    << "Matrix default constructor called." 
+    << std::endl; 
+    #endif 
+  }; 
+  ~Matrix() // destructor
+  { 
+    #ifdef DEBUG_LOG
+    std::cout 
+    << "Matrix destructor called." 
+    << std::endl; 
+    #endif 
+  };   
+
+  Matrix(const Matrix&);              // copy constructor
+  Matrix& operator=(const Matrix&);   // copy assignment
   Matrix(Matrix&&) noexcept;                 // move constructor
   Matrix& operator=(Matrix&&) noexcept;      // move assignment
-  ~Matrix() { std::cout << "Matrix destructor called." << std::endl; }; // destructor
+
+
 
   /// Other Constructors
   
@@ -36,9 +54,15 @@ public:
   /// @param rows Number of rows
   /// @param cols Number of columns
   Matrix(tQR_project::size_type rows, tQR_project::size_type cols) 
-    : numR_(rows), numC_(cols), data_(rows * cols, static_cast<tQR_project::elem_type>(0)) 
+    : 
+    numR_(rows), numC_(cols), 
+    data_(rows * cols, static_cast<tQR_project::elem_type>(0)) 
   {
-    std::cout << "Matrix parameterized constructor called." << std::endl;
+    #ifdef DEBUG_LOG
+    std::cout 
+    << "Matrix parameterized constructor called." 
+    << std::endl;
+    #endif
   }
 
   /// @brief Parameterized constructor from dimensions and initial value
@@ -46,9 +70,16 @@ public:
   /// @param cols Number of columns
   /// @param value Initial value to fill
   Matrix(tQR_project::size_type rows, tQR_project::size_type cols, tQR_project::elem_type value) 
-    : numR_(rows), numC_(cols), data_(rows * cols, value) 
+    : 
+    numR_(rows), 
+    numC_(cols), 
+    data_(rows * cols, value) 
   {
-    std::cout << "Matrix parameterized constructor with value called." << std::endl;
+    #ifdef DEBUG_LOG
+    std::cout 
+    << "Matrix parameterized constructor with value called." 
+    << std::endl;
+    #endif
   }
   
   ///
@@ -57,7 +88,9 @@ public:
   ///
   Matrix(const DataHandler& dh) 
   {
+    #ifdef DEBUG_LOG
     std::cout << "Matrix parameterized constructor called." << std::endl;
+    #endif
     numR_ = dh.GetNumRows();
     numC_ = dh.GetNumCols();
     data_ = std::vector<tQR_project::elem_type>(dh.GetDataPointer(), 
@@ -66,17 +99,34 @@ public:
 
   /// Other member functions to be implemented as needed
 
-  tQR_project::size_type GetNumElements() const { return data_.size(); }
-  tQR_project::size_type GetNumRows()     const { return numR_; }
-  tQR_project::size_type GetNumCols()     const { return numC_; }
+  tQR_project::size_type 
+  GetNumElements() 
+  const { return data_.size(); }
+  
+  tQR_project::size_type 
+  GetNumRows()     
+  const { return numR_; }
+  
+  tQR_project::size_type 
+  GetNumCols()     
+  const { return numC_; }
 
   // << and Show
-  tQR_project::Void Show() const 
+  
+  ///
+  /// @brief Print matrix to console
+  /// @return tQR_project::Void
+  ///
+  tQR_project::Void 
+  Show() const 
   {
+    #ifdef DEBUG_LOG
     std::cout << "Matrix show function called." << std::endl;
     std::cout << "Number of Rows: " << numR_ << std::endl;
     std::cout << "Number of Columns: " << numC_ << std::endl;
     std::cout << "Data: " << std::endl;
+    #endif
+
     for (tQR_project::size_type i = 0; i < numR_; ++i) {
       for (tQR_project::size_type j = 0; j < numC_; ++j) {
         std::cout << data_[i * numC_ + j] << " ";
@@ -87,27 +137,58 @@ public:
   };
 
 
-
+  ///
+  /// @brief Print matrix to output stream
+  /// @param os Output stream
+  /// @param mt Matrix to print
+  /// @return Output stream
+  ///
   friend std::ostream & operator<<(std::ostream &os, const Matrix &mt) {
-    os << "Matrix: " << mt.numR_ << " x " << mt.numC_ << std::endl;
+    os << std::endl;
     for (tQR_project::size_type i = 0; i < mt.numR_; ++i) {
       for (tQR_project::size_type j = 0; j < mt.numC_; ++j) {
         os << mt.data_[i * mt.numC_ + j] << " ";
       }
       os << std::endl;
     }
+    os << "(" << mt.numR_ << " x " << mt.numC_ << ")";
     return os;
   }
 
-  // ptr
-  tQR_project::elem_type* GetDataPointer() noexcept { return data_.data(); }
-  const tQR_project::elem_type* GetDataPointer() const noexcept { return data_.data(); }  
+  // ptr operations
+  tQR_project::elem_type* 
+  GetDataPointer() 
+  noexcept { return data_.data(); }
+  
+  const tQR_project::elem_type* 
+  GetDataPointer() 
+  const noexcept { return data_.data(); }  
 
-  // elem
-  tQR_project::elem_type& GetElement(tQR_project::size_type row, tQR_project::size_type col) noexcept 
-  { return data_[row * numC_ + col]; }
-  const tQR_project::elem_type& GetElement(tQR_project::size_type row, tQR_project::size_type col) const noexcept 
-  { return data_[row * numC_ + col]; }
+  tQR_project::elem_type* 
+  RowPtr(tQR_project::size_type r) 
+  noexcept { return data_.data() + r * numC_; }
+  
+  const tQR_project::elem_type* 
+  RowPtr(tQR_project::size_type r) 
+  const noexcept { return data_.data() + r * numC_; }
+  
+  ///
+  /// @brief Refers to the size of the Major Dimension (Row)
+  /// @return Leading dimension (Row Major)
+  ///
+  tQR_project::size_type 
+  LdaRowMajor() // Leading Dimension (Row Major)
+  const noexcept { return numC_; }
+
+
+  // elem operations
+  tQR_project::elem_type& 
+  GetElement(tQR_project::size_type row, tQR_project::size_type col) 
+  noexcept { return data_[row * numC_ + col]; }
+
+  const tQR_project::elem_type& 
+  GetElement(tQR_project::size_type row, tQR_project::size_type col) 
+  const noexcept { return data_[row * numC_ + col]; }
 
 
 
