5a03f9bf63f7010cfae88b8c722943c7388df5a3
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-20 14:47:08 +0800

    seq.main.mid.test

--- main.cc
+++ main.cc
@@ -8,24 +8,90 @@
 /// @version 0.1
 /// @date 2026-01-19 
 ///
+///
 
+
+/// includes libs + env
 #include "env/params.h"
 #include "lib/helpers.h"
 #include "lib/datahandler.h"
 #include "lib/matrix.h"
 
-
+/// includes LAPACK + C-BLAS
 #include <lapacke.h>
 #include <cblas.h>
 
 
-
 int main ()
 {
+  std::cout << "Sequential program is running. " << std::endl;
+  std::cout << "Vector size V: " << tQR_project::size_type(MATC) << std::endl;
+  std::cout << "Matrix size MATR x MATC: " << tQR_project::size_type(MATR) 
+            << " x " << tQR_project::size_type(MATC) << std::endl
+            << std::endl;            
+  // tQR_project::sleeper(1);          
   
+  tQR_project::String fn {"../data/test.txt"};
+  DataHandler dh {fn};
+  std::cout << "Data loaded from file: " << fn << std::endl;
+  // tQR_project::sleeper(1);
+
+  DataHandler dh_b = dh.Split_head(MATR);
+  dh.Reshape(MATR, MATC);
+
+  Matrix mt_A(dh);
+  Matrix mt_b(dh_b);
+  Matrix mt_tau(1, MATC, 0);
+  
+  // tQR_project::sleeper(1);
+
+  {
+    LAPACKE_dgeqrf(LAPACK_ROW_MAJOR, 
+      static_cast<tQR_project::Int>(dh.GetNumRows()), 
+      static_cast<tQR_project::Int>(dh.GetNumCols()), 
+      mt_A.GetDataPointer(),      // Placeholder for data pointer
+      static_cast<tQR_project::Int>(dh.GetNumCols()), 
+      mt_tau.GetDataPointer()   // Placeholder for tau pointer
+    );
+
+    LAPACKE_dormqr(LAPACK_ROW_MAJOR, 'L', 'T', 
+      static_cast<tQR_project::Int>(dh_b.GetNumRows()), 
+      static_cast<tQR_project::Int>(1), 
+      static_cast<tQR_project::Int>(dh.GetNumCols()), 
+      mt_A.GetDataPointer(),
+      static_cast<tQR_project::Int>(dh.GetNumCols()),
+      mt_tau.GetDataPointer(),
+      mt_b.GetDataPointer(),
+      static_cast<tQR_project::Int>(1)
+    );
+  }  
+  // tQR_project::sleeper(1);
 
 
+  /// Get Result of 'x'
+  Matrix R(MATC, MATC, 0);
+  for (int i = 0; i < MATC; ++i) {
+    for (int j = i; j < MATC; ++j) {
+      R.GetElement(i,j) = mt_A.GetElement(i,j);
+    }
+  }
 
+  Matrix x(MATC, 1, 0);
+  for (int i = 0; i < MATC; ++i) x.GetElement(i,0) = mt_b.GetElement(i,0);
+
+  tQR_project::Int info = LAPACKE_dtrtrs(
+    LAPACK_ROW_MAJOR,
+    'U',   // upper triangular
+    'N',   // No transpose
+    'N',   // Non-unit diagonal
+    MATC,  // Column of Matrix
+    1,     // nrhs
+    R.GetDataPointer(),
+    MATC,  // lda = n in row-major for n×n
+    x.GetDataPointer(),
+    1      // ldb = nrhs in row-major
+  );
+  if (info != 0) { /* handle */ }  
 
 
 
