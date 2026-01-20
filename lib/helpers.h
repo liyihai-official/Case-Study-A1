8acfc431e9371284f00b33f68285524cae18efda
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-20 15:01:51 +0800

    typo.fix

--- lib/helpers.h
+++ lib/helpers.h
@@ -14,18 +14,6 @@ namespace tQR_project {
     sleep(seconds);
     std::cout << std::endl;
   }
-  // std::ostream & operator<<(std::ostream &os, const DataHandler &dh) {
-  //   os << "DataHandler: " << dh.GetNumRows() << " x " << dh.GetNumCols() 
-  //      << ", Total elements: " << dh.GetNumElements() << std::endl;
-  //   for (tQR_project::size_type i = 0; i < dh.GetNumRows(); ++i) {
-  //     for (tQR_project::size_type j = 0; j < dh.GetNumCols(); ++j) {
-  //       os << dh.GetElement(i, j) << " ";
-  //     }
-  //     os << std::endl;
-  //   }
-  //   return os;
-  // }
-
 } // end of namespace tQR_project
 
 
