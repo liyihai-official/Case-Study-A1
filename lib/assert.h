78ab1147940eb0f0660bd5e5473efd56668ae40e
Parley <47943866+liyihai-official@users.noreply.github.com>
2026-01-21 10:25:18 +0800

    add.assert

---
+++ lib/assert.h
@@ -0,0 +1,38 @@
+///
+///
+///
+///
+#ifndef ASSERT_H
+#define ASSERT_H
+
+/// includes
+#include <cassert>
+#include <iostream>
+#include <cstdlib>
+#include "../env/params.h"
+
+
+/// @brief Logging macro for debugging
+/// @param msg Message to log
+#if DEBUG_LOG
+#define LOG(msg) \
+  std::cout << "[LOG] " << msg << std::endl;
+#else 
+  #define LOG(msg) \
+  do {} while(0)
+#endif
+
+
+
+/// @brief Custom assert macro for debugging
+/// @param expr Expression to assert
+#define TSQR_ASSERT(expr) \
+  if (!(expr)) { \
+    std::cerr << "Assertion Failed: " #expr \
+              << ", file " << __FILE__ \
+              << ", line " << __LINE__ \
+              << std::endl; \
+    assert(expr); \
+  }
+
+#endif // ASSERT_H
